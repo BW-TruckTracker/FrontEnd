2a66d32d377b7fb2c99fe9c7d3a4854f43b6d38d
unknown <gradybollas@gmail.com>
2021-01-05 14:25:28 -0800

    main change was seperating the Form section of login, and the Login Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Homepage from './components/homepage/Homepage'
+import Homepage from './components/homepage/Homepage';
+import Login from './components/login/Login';
 
 function App() {
   return (
