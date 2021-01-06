90c2b2873433e6784622d07bffad1a64608128f9
Michelle Chen <mchen0062@gmail.com>
2021-01-06 14:03:05 -0800

    Created axios request for trucks, passed down to TruckCard

--- src/components/foodtruck/FoodTruck.js
+++ src/components/foodtruck/FoodTruck.js
@@ -2,17 +2,31 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from 'styled-components'; 
 
-//* Route to 'more info button' on homepage or to popup
-// Remember to pass down truck props to FoodTruck and TruckCard
-
 const DirectionsButton = styled.button`
 background-color: gold;
 color: white;
 `
+
+const baseUrl = "https://unit4-build-week-backend.herokuapp.com/api";
+const truckUrl = "trucks"; 
+
+// need to use useParams
+// const { id } = useParams(); 
+// Then in: <Route path='/reviews/:id'> (for which component?)
+
 export default function FoodTruck() {
+
+const [truck, setTruck] = useState({}); 
+
+useEffect(() => {
+        axios.get(`${baseUrl}/${truckUrl}`).then(res => setTruck(res.data)).catch(err => console.log('Error from TruckCard endpoint'))
+        }, []);
+
+//set data to state 
+
     return (
         <>
-        <TruckCard />    
+        <TruckCard truck={truck} />    
         <DirectionsButton>Get Directions</DirectionsButton>
         </>
     )
