350181accb3fbd455d80576e29daf559d344f6af
duointherain <duointherain@gmail.com>
2021-01-06 14:22:03 -0800

    update routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import React from 'react';
 import SignUp from './components/SignUp';
 import About from './components/About';
+import Login from './components/login/Login';
+
 
 
 function App() {
@@ -15,11 +17,11 @@ function App() {
       <Router>
       
       <Switch>
-       
+        <Route exact path="/" component={About} />
         <Route path="/SignUp" component={SignUp} />
         <Route path="/Homepage" component={Homepage} />
+        <Route path="/Login" component={Login} />
         
-        <Route path="/About" component={About} />
         
       </Switch>
 
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import LoginForm from './components/LoginForm'
+import LoginForm from './LoginForm'
 
 function Login() {
   const adminUser = {
