96ae5b4970c0da3f83c38c6ad852f91541246d43
duointherain <duointherain@gmail.com>
2021-01-06 21:35:45 -0800

    review form complete

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,20 +1,67 @@
 import React from 'react';
 import Navbar from '../components/Navbar.js';
-import Footer from '../components/footer/Footer.js';
+
 import logor from '../images/reviewlogo.png'
 import '../App.css';
 import {BrowserRouter as Link} from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
 
 
-
+const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
 
 export default function About() {
   return (
     <div className="main">
         <Navbar />
-        <div className='central'>
-        <h1>Your voice Counts! Leave a review!  </h1>
+        <div></div>
         <img src={logor} alt="Review logo" />
+        <div className='central'>
+         <div>
+            <form>
+                <div className="bar">
+                <h3>Food</h3>
+                <ReactStars
+                     count={5}
+                     onChange={ratingChanged}
+                    size={24}
+                    activeColor="#ffd700"
+                     />
+                </div>
+
+                <div className="bar">
+                <h3>Hygiene</h3>
+                <ReactStars
+                     count={5}
+                     onChange={ratingChanged}
+                    size={24}
+                    activeColor="#ffd700"
+                     />
+                </div>
+                <div className="bar">
+                <h3>Service</h3>
+                <ReactStars
+                     count={5}
+                     onChange={ratingChanged}
+                    size={24}
+                    activeColor="#ffd700"
+                     />
+                </div>
+                <div >
+                <h3>Leave a Comment:</h3>
+                <textarea id="" name="" rows="7" cols="25">
+
+                </textarea>
+                </div>
+                <div>
+                <button disabled='true'>Submit</button>
+                <button>Cancel</button>
+
+                </div>
+            </form>
+            </div>
+      
         </div>
         
       
@@ -23,8 +70,9 @@ export default function About() {
  
 
 
-
-     
+<footer><Navbar /></footer>
+        
     </div>
+     
   );
 }
