84e6f1301db599dc2950b6bba86e11dd83a37622
Michelle Chen <mchen0062@gmail.com>
2021-01-07 15:32:24 -0800

    Render Favorites component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-import './App.css';
-import Homepage from './components/homepage/Homepage'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import React from 'react';
-import SignUp from './components/SignUp';
-import About from './components/About';
-import Favorites from './components/foodtruck/Favorites'; 
+import "./App.css";
+import Homepage from "./components/homepage/Homepage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import SignUp from "./components/SignUp";
+import About from "./components/About";
+import Favorites from "./components/foodtruck/Favorites";
 
 function App() {
   return (
     <main>
-    
-
-
       <Router>
-      
-      <Switch>
-       
-        <Route path="/SignUp" component={SignUp} />
-        <Route path="/Homepage" component={Homepage} />
+        <Switch>
+          <Route path="/SignUp" component={SignUp} />
+          <Route path="/Homepage" component={Homepage} />
 
-        <Route path="/About" component={About} />
-        <Route path="/Favorites" component={Favorites}/>
-        
-      </Switch>
-
+          <Route path="/About" component={About} />
+          <Route path="/Favorites" component={Favorites} />
+        </Switch>
       </Router>
-
-   
     </main>
-    
   );
 }
 
