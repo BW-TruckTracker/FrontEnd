668f1beb15f62408f9256cafd56dc6716e061b5c
Michelle Chen <mchen0062@gmail.com>
2021-01-07 00:33:11 -0800

    axios request for trucks and rendered them on click

--- src/components/foodtruck/Favorites.js
+++ src/components/foodtruck/Favorites.js
@@ -9,23 +9,15 @@ background: brown;
 font-weight: bold; 
 `
 const FaveTruckCard = styled.div`
-border-radius: 10%;
 background: salmon; 
 `
-const TruckCardRight = styled.div`
-border: gray; 
-`
-const TruckCardLeft = styled.div`
-border: gray; 
-`
+
 const RemoveButton = styled.button`
 border: red;
 display: inline-block; 
 color: red; 
 `
 function Favorites (props){
-
-  // const [fave, setFave] = useState([]);
 
   useEffect(() => {
     axios
@@ -44,14 +36,9 @@ return (
         <h2>Your Favorite Trucks</h2>
     <FaveTruckCard>
     {console.log('HERE')}
-    <TruckCardRight>
       <h2>(truck.truck_name)</h2>
       <span>Stars component</span>
       <h3>(truck.cuisine_type)</h3>
-    </TruckCardRight>
-    <TruckCardLeft>
-      {/* An image goes inside this div (truck.truck_img_url*/}
-    </TruckCardLeft>
     <RemoveButton>x</RemoveButton>
   </FaveTruckCard>
     </FavoritesComponent>
--- src/components/foodtruck/FoodTruck.js
+++ src/components/foodtruck/FoodTruck.js
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import styled from 'styled-components'; 
 import TruckCard from './TruckCard.js';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
 
 const DirectionsButton = styled.button`
-background-color: gold;
+background-color: #edca1a;
+border: solid 1px white; 
 color: white;
 `
 
-const baseUrl = "https://unit4-build-week-backend.herokuapp.com/api";
-const trucksUrl = "trucks"; 
-
-export default function FoodTruck() {
-
-const [trucks, setTrucks] = useState({}); 
-
-useEffect(() => {
-        axios.get(`${baseUrl}/${trucksUrl}`).then(res => setTrucks(res.data)).catch(err => console.log('Error from TruckCard endpoint'))
-        }, []);
+export default function FoodTruck(props) {
+    const { activeTruck } = props; 
+    const truckUrl = "https://foodtruck-backend-api.herokuapp.com/api/trucks";
+    const truckDetails = activeTruck; 
+    const [truck, setTrucks] = useState([]);
+    
+    useEffect(() => {
+            axios.get(`${truckUrl}/${truckDetails}`).then(res => setTrucks(res.data)).catch(err => console.log('Request error from BaseMap'))
+            });
 
     return (
         <>
-        <TruckCard trucks={trucks} />  
+        <TruckCard truck={truck}/>
         <DirectionsButton>Get Directions</DirectionsButton>
         </>
     )
