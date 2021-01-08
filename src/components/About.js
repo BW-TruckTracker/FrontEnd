1ffac1d89d75326f2de095846a466beb2eddb08a
Michelle Chen <mchen0062@gmail.com>
2021-01-07 20:12:51 -0800

    Link About to map (homepage)

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar.js';
 import Footer from '../components/footer/Footer.js';
 import logo1 from '../images/logopng.png'
 import '../App.css';
-import {BrowserRouter as Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 
 
@@ -16,16 +16,8 @@ export default function About() {
         <h1>We're glad you're here! </h1>
         <img src={logo1} alt="Go'them trucks stylized bat New York City Food Trucks" />
         </div>
-        <Link to="/Homepage">
-        <div className="enter">Enter to Find Great Food!</div>
-        </Link>
+        <Link to="/Homepage"><div className="enter">Enter to Find Great Food!</div></Link>
         {/* <a href={/Homepage}>enter</a> */}
-      
-
-        
- 
-
-
 
      <Footer />
     </div>
